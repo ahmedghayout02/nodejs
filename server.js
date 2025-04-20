0ec3845ea4245be3cc7d19f3cbbb24a4ea4679b8
ahmedghayout02 <ahmedgaot@gmail.com>
2025-04-20 19:09:06 +0100

    تحسين الخادم

--- server.js
+++ server.js
@@ -15,8 +15,9 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+  });
 
 // التحقق من الاتصال بقاعدة البيانات
 db.connect(err => {
